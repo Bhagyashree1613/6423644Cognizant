7653691d265c01437172488b1b952d18653da6de
K M Bhagyashree <bhagyashree8016@gmail.com>
2025-08-02 21:45:17 +0530

    Week 6 Additional Hands On Exercises
    Adding week 6 Additional Hands On Exercise 8

---
+++ Week 6 React Hands On/8. ReactJS-HOL/Code/CountPeople.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class CountPeople extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      entryCount: 0,
+      exitCount: 0,
+    };
+  }
+
+  updateEntry = () => {
+    this.setState({ entryCount: this.state.entryCount + 1 });
+  };
+
+  updateExit = () => {
+    this.setState({ exitCount: this.state.exitCount + 1 });
+  };
+
+  render() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h1 style={{ color: 'green' }}>Mall Entry & Exit Count</h1>
+        <h2>People Entered: {this.state.entryCount}</h2>
+        <h2>People Exited: {this.state.exitCount}</h2>
+        <button onClick={this.updateEntry} style={{ margin: '10px', padding: '10px' }}>
+          Login
+        </button>
+        <button onClick={this.updateExit} style={{ margin: '10px', padding: '10px' }}>
+          Exit
+        </button>
+      </div>
+    );
+  }
+}
+
+export default CountPeople;
